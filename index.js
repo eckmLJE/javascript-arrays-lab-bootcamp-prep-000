81acaa198cc4c6ed88f8a1d11ecd75a5128061f5
Lucas Eckman <lucas.eckman@gmail.com>
2018-03-07 23:30:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  var myArray = kittens.slice(0, kittens.slice - 1);
+  var myArray = kittens.slice(0, kittens.length - 1);
   return myArray
 }
